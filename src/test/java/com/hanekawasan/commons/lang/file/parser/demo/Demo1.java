af239d64dc22e1f3dc3734b5f58d646fae365f72
yukms <763803382@qq.com>
2019-04-28 20:25:40 +0800

    JavaParser

---
+++ src/test/java/com/hanekawasan/commons/lang/file/parser/demo/Demo1.java
@@ -0,0 +1,13 @@
+/*
+声明
+ */
+package com.hanekawasan.commons.lang.file.parser.demo;
+
+import com.hanekawasan.commons.lang.file.parser.demo.annotation.AnnotationDemo;
+
+/**
+ * @author yukms 763803382@qq.com 2019/4/28 19:38
+ */
+@AnnotationDemo
+public class Demo1 {
+}
--- src/test/java/com/hanekawasan/commons/lang/file/parser/demo/annotation/AnnotationDemo.java
+++ src/test/java/com/hanekawasan/commons/lang/file/parser/demo/annotation/AnnotationDemo.java
@@ -1,4 +1,4 @@
-package com.hanekawasan.commons.lang.file.parser;
+package com.hanekawasan.commons.lang.file.parser.demo.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
