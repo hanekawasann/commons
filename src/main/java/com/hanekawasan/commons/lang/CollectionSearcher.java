e40ca563d44da575eff129308fc73d80d0b0ec71
yukms <763803382@qq.com>
2019-11-04 15:29:05 +0800

    timer

---
+++ src/main/java/com/hanekawasan/commons/lang/CollectionSearcher.java
@@ -0,0 +1,31 @@
+package com.hanekawasan.commons.lang;
+
+import java.util.Collection;
+import java.util.TreeMap;
+import java.util.function.Function;
+
+/**
+ * 集合查找器
+ *
+ * @author yukms 2019/11/4
+ */
+public final class CollectionSearcher<T, R> {
+    private TreeMap<T, R> map;
+
+    private CollectionSearcher(TreeMap<T, R> map) {
+        this.map = map;
+    }
+
+    public R get(T t) {
+        return map.get(t);
+    }
+
+    public static <T, R> CollectionSearcher<T, R> newInstance(Collection<R> collection,
+        Function<? super R, T> function) {
+        TreeMap<T, R> map = new TreeMap<>();
+        for (R r : collection) {
+            map.put(function.apply(r), r);
+        }
+        return new CollectionSearcher<>(map);
+    }
+}
